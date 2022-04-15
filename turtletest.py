1df64553d8a1d9f61ce6c817cab377163a5d8ed4
Saltloaf <76228985+Saltloaf@users.noreply.github.com>
2022-04-15 15:21:50 -0400

    Instantiates clones at the center of each grid square
    and adds them to the nested list

--- turtletest.py
+++ turtletest.py
@@ -1,13 +1,14 @@
 import turtle
+
 t = turtle.Turtle()
 s = turtle.Screen()
 
 # I'm assuming we're just going to be using square-shaped grid spaces
 rolumns = 10 # rolumns = rows & columns
-dimension = 500 # the total height and length of grid 
+dimension = 1000 # the total height and length of grid 
 halfdimension = dimension/2
 
-t.speed(100)
+t.speed(100000)
 t.penup()
 t.goto(-halfdimension,-halfdimension) #bottom left corner
 t.pendown()
@@ -28,5 +29,19 @@ for i in range(rolumns):
 	t.pendown()
 	t.fd(dimension)
 	t.lt(90)
+#creating a nested list for referring to turtles' positions
+turtles = []
+turtlesy = []
+t.penup()
+#creates a new turtle clone each column down and adds them to the list
+for a in range(rolumns):
+	t.setpos(-halfdimension + (.5 + a)*(dimension/rolumns), 
+			halfdimension - .5*(dimension/rolumns))
+	for b in range(rolumns):
+		t.sety(halfdimension - (.5 + b)*(dimension/rolumns))
+		turtlesy.append(t.clone())
+	turtles.append(turtlesy)
+	turtlesy = []
+print(turtles[0][0])
 
 turtle.done()
