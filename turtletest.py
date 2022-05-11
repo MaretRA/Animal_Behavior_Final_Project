60e0463ae4fb91e90164c563be409ad8dd4ad7c3
Saltloaf <76228985+Saltloaf@users.noreply.github.com>
2022-05-11 01:34:48 -0400

    Cleaned up code, added clear grid
    just testing out the ui
    -setup function
    -clear grid button

--- turtletest.py
+++ turtletest.py
@@ -1,10 +1,10 @@
 import turtle
-import PyQt5 #in case we find a way to use it
+#import PyQt5 #in case we find a way to use it
 import tkinter as tk #apparently turtles only really works with tkinter
 import random
 from tkinter import *
-import numpy
-import matplotlib.pyplot as plot
+#import numpy
+#import matplotlib.pyplot as plot
 
 # I'm assuming we're just going to be using square-shaped grid spaces
 rolumns = 10 # rolumns = rows & columns
@@ -13,8 +13,7 @@ halfdimension = dimension/2
 boxwidth = dimension/rolumns
 ui_offset = 100
 #creating a nested list for referring to grid turtles' positions
-pos = []
-ypos = []
+pos = [] 
 #grid turtles will simply "spawn" the functioning turtles at their locations
 gridturtles = []
 occupiedspace = []
@@ -53,6 +52,50 @@ def randomspawnonce():
 def makemodule(coefficientx, coefficienty, module):
 	canvas.create_window(-halfdimension-(1+coefficientx)*ui_offset, 
 		-halfdimension+coefficienty*ui_offset, window =module)
+def setup():
+	state.set('Creating Grid')
+	t = turtle.Turtle()
+	t.hideturtle()
+	t.speed(100000)
+	t.penup()
+	t.goto(-halfdimension,-halfdimension) #bottom left corner
+	t.pendown()
+
+	#draws the outer bounds
+	for i in range(4):
+		t.fd(dimension)
+		t.lt(90)
+	#draws the grid lines
+	for i in range(rolumns):
+		t.penup()
+		t.setpos(-halfdimension ,-halfdimension + i*(boxwidth))
+		t.pendown()
+		t.fd(dimension)
+		t.rt(90)
+		t.penup()
+		t.setpos(-halfdimension + i*(boxwidth),halfdimension)
+		t.pendown()
+		t.fd(dimension)
+		t.lt(90)
+	t.penup()
+	#creates a new turtle clone each column down and adds them to the list
+	for a in range(rolumns):
+		ypos = []
+		t.setpos(-halfdimension + (.5 + a)*(boxwidth), 
+				halfdimension - .5*(boxwidth))
+		for b in range(rolumns):
+			t.sety(halfdimension - (.5 + b)*(boxwidth))
+			ypos.append(t.clone())
+			gridturtles.append(t.clone())
+		pos.append(ypos)
+	state.set("Ready to Spawn")
+	print(len(pos))
+def cleargrid():
+	for activeturtle in activeturtles:
+		activeturtle['object'].hideturtle()
+	activeturtles.clear()
+	occupiedspace.clear()
+
 
 if __name__ == "__main__":
     screen = turtle.Screen()
@@ -61,13 +104,15 @@ if __name__ == "__main__":
     screen.bgcolor("white")
     canvas = screen.getcanvas()
 
-    button = tk.Button(canvas.master, text="Randomly Spawn", command=randomspawn)
+    state = tk.StringVar(canvas.master, 'Loading')
+    simulationstate = tk.Label (canvas.master, textvariable =state)
+
+    spawnbutton = tk.Button(canvas.master, text="Randomly Spawn", command=randomspawn)
+    clearbutton = tk.Button(canvas.master, text="Clear Grid", command = cleargrid)
     spawn_input = tk.Scale(canvas.master, from_ = len(gridturtles), orient=HORIZONTAL)
     spawnamount = spawn_input.get()
     population = tk.StringVar(canvas.master, '0')
     population_size = tk.Label (canvas.master, textvariable =population)
-    state = tk.StringVar(canvas.master, 'Loading')
-    simulationstate = tk.Label (canvas.master, textvariable =state)
     message = tk.StringVar(canvas.master, 'Welcome to the simulation! \n Start by spawning turtles')
     systemmessage = tk.Label (canvas.master, textvariable =message)
 
@@ -80,7 +125,7 @@ if __name__ == "__main__":
     messagelabeltext = tk.StringVar(canvas.master, 'Console')
     messagelabel = tk.Label (canvas.master, textvariable = messagelabeltext)
 
-    makemodule(0, 2.6, button)
+    makemodule(0, 2.6, spawnbutton)
     makemodule(0, 2.2, spawn_input)
     makemodule(0, 3.1, population_size)
     makemodule(0, 1.6, simulationstate)
@@ -89,6 +134,8 @@ if __name__ == "__main__":
     makemodule(0, .85, systemmessage)
     makemodule(1.25, 2.1, inputlabel)
     makemodule(1.25, .75, messagelabel)
+    makemodule(0, 5, clearbutton)
+    setup()
 
 #y = []
 #def plot_cont():
@@ -105,43 +152,5 @@ if __name__ == "__main__":
 #    a = anim.FuncAnimation(fig, update)
 #    plot.show()
 
-state.set('Creating Grid')
-t = turtle.Turtle()
-t.hideturtle()
-t.speed(100000)
-t.penup()
-t.goto(-halfdimension,-halfdimension) #bottom left corner
-t.pendown()
-
-#draws the outer bounds
-for i in range(4):
-	t.fd(dimension)
-	t.lt(90)
-#draws the grid lines
-for i in range(rolumns):
-	t.penup()
-	t.setpos(-halfdimension ,-halfdimension + i*(boxwidth))
-	t.pendown()
-	t.fd(dimension)
-	t.rt(90)
-	t.penup()
-	t.setpos(-halfdimension + i*(boxwidth),halfdimension)
-	t.pendown()
-	t.fd(dimension)
-	t.lt(90)
-t.penup()
-#creates a new turtle clone each column down and adds them to the list
-for a in range(rolumns):
-	t.setpos(-halfdimension + (.5 + a)*(boxwidth), 
-			halfdimension - .5*(boxwidth))
-	for b in range(rolumns):
-		t.sety(halfdimension - (.5 + b)*(boxwidth))
-		ypos.append(t.clone())
-		gridturtles.append(t.clone())
-	pos.append(ypos)
-	ypos = []
-
-
-state.set("Ready to Spawn")
 
 turtle.done()
